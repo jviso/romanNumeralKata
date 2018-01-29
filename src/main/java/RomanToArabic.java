82efac0e0daac69d20bc6fc425f730b2d2f7be12
Jacob Visovatti <jvisovatti@Jacob-Visovatti.local>
2018-01-29 13:33:21 -0500

    given II, calculate returns 2

--- src/main/java/RomanToArabic.java
+++ src/main/java/RomanToArabic.java
@@ -1,6 +1,9 @@
 public class RomanToArabic {
 
   public int calculate(String numeral) {
+      if (numeral.equals("II")) {
+          return 2;
+      }
       return 1;
   }
 
--- src/test/java/RomanToArabicTest.java
+++ src/test/java/RomanToArabicTest.java
@@ -3,10 +3,16 @@ import static org.junit.Assert.assertEquals;
 
 public class RomanToArabicTest {
 
-  @Test
-  public void calculateShouldReturn1forI() {
-      RomanToArabic romanToArabic = new RomanToArabic();
-      assertEquals(1, romanToArabic.calculate("I"));
-  }
+    @Test
+    public void calculateShouldReturn1forI() {
+        RomanToArabic romanToArabic = new RomanToArabic();
+        assertEquals(1, romanToArabic.calculate("I"));
+    }
+
+    @Test
+    public void calculateShouldReturn2ForII() {
+        RomanToArabic romanToArabic = new RomanToArabic();
+        assertEquals(2, romanToArabic.calculate("II"));
+    }
 
 }
