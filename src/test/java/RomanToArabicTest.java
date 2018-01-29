489162dd98fa4eea657f0685beaaed828a52b81f
Jacob Visovatti <jvisovatti@Jacob-Visovatti.local>
2018-01-29 14:03:26 -0500

    refactor: extract object instantiation from tests to setUp method

--- src/test/java/RomanToArabicTest.java
+++ src/test/java/RomanToArabicTest.java
@@ -1,23 +1,28 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class RomanToArabicTest {
 
+    RomanToArabic romanToArabic;
+
+    @Before
+    public void setUp() {
+        romanToArabic = new RomanToArabic();
+    }
+
     @Test
     public void calculateShouldReturn1forI() {
-        RomanToArabic romanToArabic = new RomanToArabic();
         assertEquals(1, romanToArabic.calculate("I"));
     }
 
     @Test
     public void calculateShouldReturn2ForII() {
-        RomanToArabic romanToArabic = new RomanToArabic();
         assertEquals(2, romanToArabic.calculate("II"));
     }
 
     @Test
     public void calculateShouldReturn3ForIII() {
-        RomanToArabic romanToArabic = new RomanToArabic();
         assertEquals(3, romanToArabic.calculate("III"));
     }
 
